4cf63c499b2e34d33d908ab961146d31449af7e2
emmet-lv99 <dachanisfull@gmail.com>
2022-08-01 16:52:35 +0900

    connect mongoDB

--- mapStudyNxt/model/location.js
+++ mapStudyNxt/model/location.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Shema
+const Schema = mongoose.Schema
 
 const locationSchema = new Schema({
   title: { type: String, required: true },
--- mapStudyNxt/routes/index.js
+++ mapStudyNxt/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express")
 var router = express.Router()
+const locationModel = require("../model/location")
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
@@ -8,6 +9,27 @@ router.get("/", (req, res, next) => {
 
 router.get("/upload", (req, res, next) => {
   res.render("upload", { title: "upload" })
+})
+
+router.post("/location", (req, res, next) => {
+  const { title, address, lat, lng } = req.body
+  let location = new locationModel()
+  location.title = title
+  location.address = address
+  location.lat = lat
+  location.lng = lng
+  location
+    .save()
+    .then((result) => {
+      console.log(result)
+      res.json({
+        message: "success",
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+      res.json({ message: "error" })
+    })
 })
 
 module.exports = router

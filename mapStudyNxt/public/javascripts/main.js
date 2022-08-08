dfa0c5c2c9de396698a97384be6b6230beb80271
emmet-lv99 <dachanisfull@gmail.com>
2022-08-09 08:56:47 +0900

    add line

--- mapStudyNxt/public/javascripts/main.js
+++ mapStudyNxt/public/javascripts/main.js
@@ -66,4 +66,103 @@ $.ajax({
     naver.maps.Event.addListener(markerList[i], "click", getClickHandler(i))
     naver.maps.Event.addListener(map, "click", getClickMap(i))
   }
+
+  const cluster1 = { content: `<div class="cluster1"></div>` }
+  const cluster2 = { content: `<div class="cluster2"></div>` }
+  const cluster3 = { content: `<div class="cluster3"></div>` }
+
+  const markerClustering = new MarkerClustering({
+    minClusterSize: 2,
+    maxZoom: 12,
+    map: map,
+    markers: markerList,
+    disableClickZoom: false,
+    gridSize: 20,
+    icons: [cluster1, cluster2, cluster3],
+    indexGernerator: [2, 5, 10],
+    stylingFunction: (clusterMarker, count) => {
+      $(clusterMarker.getElement()).find("div:first-child").text(count)
+    },
+  })
 })
+
+const urlPrefix = "https://navermaps.github.io/maps.js/docs/data/region"
+const urlSuffix = ".json"
+
+let regionGeoJson = []
+let loadCount = 0
+
+const tooltip = $(
+  `<div style="position: absolute; z-index:1000; padding: 5px 10px; background: white; border: 1px solid black; font-size: 14px; display: none; pointer-events: none;"></div>`
+)
+
+tooltip.appendTo(map.getPanes().floatPane)
+
+naver.maps.Event.once(map, "init_stylemap", () => {
+  for (let i = 1; i < 18; i++) {
+    let keyword = i.toString()
+    if (keyword.length === 1) {
+      keyword = "0" + keyword
+    }
+    $.ajax({
+      url: urlPrefix + keyword + urlSuffix,
+    }).done((geojson) => {
+      regionGeoJson.push(geojson)
+      loadCount++
+      if (loadCount === 12) {
+        startDataLayer()
+      }
+    })
+  }
+})
+
+function startDataLayer() {
+  map.data.setStyle((feature) => {
+    const styleOptions = {
+      fillColor: "#ff0000",
+      fillOpacity: 0.0002,
+      strokeColor: "#ff0000",
+      strokeWeight: 2,
+    }
+    if (feature.getProperty("focus")) {
+      styleOptions.fillOpacity = 0.6
+      styleOptions.fillColor = "#0f0"
+      styleOptions.strokeColor = "#0f0"
+      styleOptions.strokeWeight = 4
+      styleOptions.strokeOpacity = 1
+    }
+    return styleOptions
+  })
+  regionGeoJson.forEach((geojson) => {
+    map.data.addGeoJson(geojson)
+  })
+
+  map.data.addListener("click", (e) => {
+    let feature = e.feature
+    if (feature.getProperty("focus") !== true) {
+      feature.setProperty("focus", true)
+    } else {
+      feature.setProperty("focus", false)
+    }
+  })
+  map.data.addListener("mouseover", (e) => {
+    let feature = e.feature
+    let regionName = feature.getProperty("area1")
+    tooltip
+      .css({
+        display: "block",
+        left: e.offset.x,
+        top: e.offset.y,
+      })
+      .text(regionName)
+    map.data.overrideStyle(feature, {
+      fillOpacity: 0.6,
+      strokeWeight: 4,
+      strokeOpacity: 1,
+    })
+  })
+  map.data.addListener("mouseout", (e) => {
+    tooltip.hide().empty()
+    map.data.revertStyle()
+  })
+}
